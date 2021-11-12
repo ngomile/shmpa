eb76eba07915c5021bf7889fd25fccb34ac38909
ngomile <ngomile932@gmail.com>
2021-11-12 09:38:21 +0200

    Defined base functions to use

---
+++ main.py
@@ -0,0 +1,40 @@
+from typing import List, NoReturn
+
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def soupify_path(path: str) -> BeautifulSoup:
+    """
+    Open the path to the file and read it into the Beautiful constructor and
+    return the result
+    """
+
+
+def extract_web_tags(soup: BeautifulSoup) -> List[str]:
+    """
+    Extract the tags from the soupified html document and return a list of
+    the tags that are in the table matching the selector tr > td:nth-child(2)
+    """
+
+
+def extract_sheet_tags(df: pd.DataFrame) -> List[str]:
+    """
+    Extract the tags from the tags column in the dataframe and return a list
+    of the tags
+    """
+
+
+def diff_loans(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a dataframe containing the farmers whose loan balances between the
+    last and recently calculated quarters, has a non zero balance
+    """
+
+
+def run() -> NoReturn:
+    pass
+
+
+if __name__ == '__main__':
+    run()
