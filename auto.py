f28177b205e5f823d3bd434010eedd84ff90ea0d
ngomile <ngomile932@gmail.com>
2022-01-15 14:58:44 +0200

    Created config dictionary for handling sheets

---
+++ auto.py
@@ -0,0 +1,22 @@
+config = {
+    'tm': {
+        'path': 'C:/Users/SHMPA Data/Documents/shmpa/fmrs TM Jul21.xlsx',
+        'sheets': {
+            'bt': {
+                'heifer_sheet': 'bth',
+            },
+            'cm': {
+                'heifer_sheet': 'cmh',
+            },
+            'mpA': {
+                'heifer_sheet': 'mpAh',
+            },
+            'mpT': {
+                'heifer_sheet': 'mpTh',
+            },
+            'ck': {
+                'heifer_sheet': 'ckh',
+            },
+        }
+    }
+}
