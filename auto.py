0b557457932b6ae7766590fc458476b6f9fd74ea
ngomile <ngomile932@gmail.com>
2022-01-21 17:04:09 +0200

    Implemented yield_as_record generator that returns a combination of dam and heifer rows using a common record

--- auto.py
+++ auto.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 from config import get_config
+from records import RowRecord
+from utils import take_cols
 
 
 class SheetHandler:
@@ -40,3 +42,14 @@ class SheetHandler:
             na_filter=False,
             skiprows=3
         )
+
+    def yield_as_record(self):
+        '''
+        Is a generator that yields the rows of both the dam sheet and the heifer sheet
+        as a RowRecord
+        '''
+        for _, row in take_cols(self.df_dams).iterrows():
+            yield row
+
+        for _, row in take_cols(self.df_heifers).iterrows():
+            yield row
