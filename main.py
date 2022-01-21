b5b16883ebaa9836d2843f532bdbdc9b6605deba
ngomile <ngomile932@gmail.com>
2022-01-21 11:47:59 +0200

    Moved logic to initialise sheet dataframes to its own class

--- main.py
+++ main.py
@@ -154,45 +154,43 @@ if __name__ == '__main__':
     import pandas as pd
     from config import get_config
 
-    config = get_config()
     document = 'tm'
     sheet = 'mpA'
 
-    dam_names = config['dam']['names']
-    dam_converters = config['dam']['converters']
-
-    dam_cols = config['dam']['cols']
-
-    path = config[document]['path']
-
-    heifer_names = config['heifer']['names']
-    heifer_sheet = config[document]['sheets'][sheet]['heifer_sheet']
-
-    heifer_cols = config['heifer']['cols']
-    sheets = config[document]['sheets']
-    heifer_cols = sheets.get(sheet).get('heifer_cols') or heifer_cols
-    heifer_converters = config['heifer']['converters']
-
-    df_dams = pd.read_excel(
-        path,
-        names=dam_names,
-        usecols=dam_cols,
-        na_filter=False,
-        converters=dam_converters,
-        sheet_name=sheet,
-        skiprows=2
-    )
-
-    df_heifers = pd.read_excel(
-        path,
-        names=heifer_names,
-        usecols=heifer_cols,
-        converters=heifer_converters,
-        sheet_name=heifer_sheet,
-        na_filter=False,
-        skiprows=3
-    )
-
     class SheetHandler:
         def __init__(self, document: str, sheet: str) -> None:
-            pass
+            config = get_config()
+            path = config[document]['path']
+            sheets = config[document]['sheets']
+
+            dam_names = config['dam']['names']
+            dam_converters = config['dam']['converters']
+            dam_cols = config['dam']['cols']
+
+            heifer_names = config['heifer']['names']
+            heifer_sheet = config[document]['sheets'][sheet]['heifer_sheet']
+            heifer_converters = config['heifer']['converters']
+
+            # In some cases some heifer sheets may have distinct column arrangement
+            heifer_cols = config['heifer']['cols']
+            heifer_cols = sheets.get(sheet).get('heifer_cols') or heifer_cols
+
+            self.df_dams = pd.read_excel(
+                path,
+                names=dam_names,
+                usecols=dam_cols,
+                na_filter=False,
+                converters=dam_converters,
+                sheet_name=sheet,
+                skiprows=2
+            )
+
+            self.df_heifers = pd.read_excel(
+                path,
+                names=heifer_names,
+                usecols=heifer_cols,
+                converters=heifer_converters,
+                sheet_name=heifer_sheet,
+                na_filter=False,
+                skiprows=3
+            )
