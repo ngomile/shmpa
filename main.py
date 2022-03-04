493181e133fee44dbddb775d869f0fc88a00be14
ngomile <ngomile932@gmail.com>
2022-03-04 09:51:35 +0200

    Moved code into dedicated main function

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-if __name__ == '__main__':
-    import time
+import time
 
-    import pandas as pd
+import pandas as pd
 
-    from auto import SheetHandler
-    from utils import OUTPUT_DIR
+from auto import SheetHandler
+from utils import OUTPUT_DIR
 
+
+def main():
     start = time.perf_counter()
     document = 'tm'
     transferred: bool = False
 
     if transferred:
-        sheets = ['mpA', 'mpT']
-        transfers = list(SheetHandler.find_all_transfers(document, sheets))
+        transfers = list(SheetHandler.find_transferred(document))
         _path = f'{OUTPUT_DIR}/transfers.xlsx'
         pd.DataFrame(transfers).to_excel(_path, index=False)
 
     end = time.perf_counter()
     print(f'That took {end - start}')
+
+
+if __name__ == '__main__':
+    main()
