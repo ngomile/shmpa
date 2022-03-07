64aadf62971f9060b81f97b349e17bcbcf4e86d5
ngomile <ngomile932@gmail.com>
2022-03-07 12:28:20 +0200

    Simplify conversion to dictionary for RowRecord dataclass

--- records.py
+++ records.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import Dict, Any
 
 import pandas as pd
@@ -170,22 +170,7 @@ class RowRecord:
         '''
         Returns the dictionary representation of the RowRecord type
         '''
-        return {
-            'tag': self.tag,
-            'farmer_name': self.farmer_name,
-            'mbg': self.mbg,
-            'code': self.code,
-            'cow_no': self.cow_no,
-            'repaid': self.repaid,
-            'events': self.events,
-            'breed': self.breed,
-            'semen': self.semen,
-            'born': self.born,
-            'date_removed': self.date_removed,
-            'destination': self.destination,
-            'status': self.status,
-            'is_dam': self.is_dam,
-        }
+        return asdict(self)
 
 
 @dataclass
